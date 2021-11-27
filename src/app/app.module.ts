d51b3dcde25bc3886f8662621cb37fbd2c09c676
Rufuslit4z <paul.marie@protonmail.com>
2021-11-27 14:27:13 +0100

    Création des composants et services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarteComponent } from './component/carte/carte.component';
+import { DeckComponent } from './component/deck/deck.component';
+import { BoutiqueComponent } from './component/boutique/boutique.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarteComponent,
+    DeckComponent,
+    BoutiqueComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
