9e7411265aba495060a5d4aa753d162c15461d64
dalmadu25 <dalmadu25@gmail.com>
2021-12-06 17:06:00 +0100

    Navigation avec routing ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoutiqueComponent } from './component/boutique/boutique.component';
 import { DeckComponent } from './component/deck/deck.component';
+import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
-  { path: 'home/deck', component: DeckComponent }
+  { path: 'home/login', component: LoginComponent },
+  { path: 'home/deck', component: DeckComponent },
+  { path: 'home/boutique', component: BoutiqueComponent },
+  { path: '',
+    redirectTo: 'home/login',
+    pathMatch: 'full' },
+  { path: '**', 
+    redirectTo: 'home/login',
+    pathMatch: 'full' }
 ];
 
 @NgModule({
